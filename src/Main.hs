d9baf37a698df0a093e29d12f9985cdac8227925
Pierre Thierry <pierre@nothos.net>
2023-12-02 18:17:36 +0100

    Implemented day 1

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
-import Day01 (day01)
-import Day02 (day02, day02_1, day02_2)
+import Day01
+import Day02
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
+    "1" : "1" : _ -> day01_1
+    "1" : "2" : _ -> day01_2
     "1" : _ -> day01
     "2" : "1" : _ -> day02_1
     "2" : "2" : _ -> day02_2
